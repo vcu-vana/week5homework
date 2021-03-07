f3d4bf42b47eb88432bc80a2b05836b89f215afd
vcu-vana <vana@vcu.edu>
2021-03-07 13:10:14 -0500

    Adding return statements to homework.py

--- fun/homework.py
+++ fun/homework.py
@@ -6,7 +6,8 @@ def find_greatest_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the largest number in the list.
     """
-    max(incoming_list)
+    greatest = max(incoming_list)
+    return(greatest)
 
 
 def find_least_number(incoming_list):
@@ -14,7 +15,8 @@ def find_least_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the smallest/least number in the list.
     """
-    min(incoming_list)
+    least = min(incoming_list)
+    return(least)
 
 
 def add_list_numbers(incoming_list):
@@ -31,4 +33,5 @@ def longest_value_key(incoming_dict):
     Required parameter, incoming_dict, should be a dict.
     Find the KEY that has a value with the highest length, use the len() function
     """
-    len(max(incoming_dict))
+    longest = len(max(incoming_dict))
+    return(longest)
