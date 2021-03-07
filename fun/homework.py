ace870db5e3aabbd0df08b5a1c7fae6b1fa3c54b
vcu-vana <vana@vcu.edu>
2021-03-07 12:51:21 -0500

    Changing back to submission 2

--- fun/homework.py
+++ fun/homework.py
@@ -6,7 +6,7 @@ def find_greatest_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the largest number in the list.
     """
-    assert greatest_number = max(incoming_list)
+    greatest_number = max(incoming_list)
     pass
 
 
@@ -15,7 +15,7 @@ def find_least_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the smallest/least number in the list.
     """
-    assert least_number = min(incoming_list)
+    least_number = min(incoming_list)
     pass
 
 
@@ -24,8 +24,8 @@ def add_list_numbers(incoming_list):
     Required parameter, incoming_list, should be a list.
     Add all the values together and return it.
     """
-    assert total = sum(incoming_list)
-    assert print(total)
+    total = sum(incoming_list)
+    print(total)
     pass
 
 
@@ -34,5 +34,5 @@ def longest_value_key(incoming_dict):
     Required parameter, incoming_dict, should be a dict.
     Find the KEY that has a value with the highest length, use the len() function
     """
-    assert longest = len(max(incoming_dict))
+    longest = len(max(incoming_dict))
     pass
