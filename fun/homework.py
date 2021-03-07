509af797e91b01f526188ef3614e52fc13cbbe78
vcu-vana <vana@vcu.edu>
2021-03-07 12:53:58 -0500

    Removed variable declaration

--- fun/homework.py
+++ fun/homework.py
@@ -6,7 +6,7 @@ def find_greatest_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the largest number in the list.
     """
-    greatest_number = max(incoming_list)
+    max(incoming_list)
     pass
 
 
@@ -15,7 +15,7 @@ def find_least_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the smallest/least number in the list.
     """
-    least_number = min(incoming_list)
+    min(incoming_list)
     pass
 
 
@@ -34,5 +34,5 @@ def longest_value_key(incoming_dict):
     Required parameter, incoming_dict, should be a dict.
     Find the KEY that has a value with the highest length, use the len() function
     """
-    longest = len(max(incoming_dict))
+    len(max(incoming_dict))
     pass
