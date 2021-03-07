4b6bf62f9959b1bf7bedfb8b7c590de3a2259582
vcu-vana <vana@vcu.edu>
2021-03-07 12:44:07 -0500

    Submission 1 for Week 5 HW

--- fun/homework.py
+++ fun/homework.py
@@ -6,6 +6,7 @@ def find_greatest_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the largest number in the list.
     """
+    greatest_number = max(incoming_list)
     pass
 
 
@@ -14,6 +15,7 @@ def find_least_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the smallest/least number in the list.
     """
+    least_number = min(incoming_list)
     pass
 
 
@@ -22,7 +24,8 @@ def add_list_numbers(incoming_list):
     Required parameter, incoming_list, should be a list.
     Add all the values together and return it.
     """
-
+    total = sum(incoming_list)
+    print(total)
     pass
 
 
@@ -31,4 +34,5 @@ def longest_value_key(incoming_dict):
     Required parameter, incoming_dict, should be a dict.
     Find the KEY that has a value with the highest length, use the len() function
     """
+    longest = len(max(incoming_dict))
     pass
